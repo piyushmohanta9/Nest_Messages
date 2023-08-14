928e0d6ce7e369ae9662d4db9861b60519a18987
Piyush Mohanta <61897664+piyushmohanta9@users.noreply.github.com>
2023-08-14 23:19:36 +0530

    Created Nest application using nest cli

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Body, Param} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+        return "Messages";
+    }
+
+    @Post()
+    createMessages(@Body() body:CreateMessageDto){
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id:String){
+        console.log(id);
+    }
+
+}
